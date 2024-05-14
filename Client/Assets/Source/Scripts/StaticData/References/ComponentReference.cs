0f755eda1c95bf17a0f69dadf407d1ea05f78cd5
Nikolai Kochergin <Kocherginizer@gmail.com>
2024-05-14 03:23:11 +0300

    Add ComponentReference

---
+++ Client/Assets/Source/Scripts/StaticData/References/ComponentReference.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Source.Scripts.StaticData.References
+{
+    [Serializable]
+    public class ComponentReference<TComponent> : AssetReference
+    {
+        public ComponentReference(string guid) : base(guid) {}
+        
+        public override bool ValidateAsset(string path)
+        {
+#if UNITY_EDITOR
+            GameObject go = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            return go != null && go.GetComponent<TComponent>() != null;
+#else
+            return false;
+#endif
+        }
+    }
+}
